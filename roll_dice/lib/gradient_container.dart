2bbb15d98cb9552453503a3a182a7d19fa2d1120
root <admin@example.com>
2023-08-10 22:19:56 +0300

    added image widget

--- roll_dice/lib/gradient_container.dart
+++ roll_dice/lib/gradient_container.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:roll_dice/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
-
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
@@ -14,15 +12,18 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration:  BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: colors,
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
+        ),
       ),
     );
   }
