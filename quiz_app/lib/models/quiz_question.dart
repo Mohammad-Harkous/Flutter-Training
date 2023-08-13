287838a3f07ff2b0a0c183052010f2a056b35f76
root <admin@example.com>
2023-08-13 23:43:45 +0300

    added shuffle functionality

--- quiz_app/lib/models/quiz_question.dart
+++ quiz_app/lib/models/quiz_question.dart
@@ -3,4 +3,11 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    // creating a copy of list answers using "List.of()" function
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
