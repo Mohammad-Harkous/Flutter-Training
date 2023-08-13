dcb3c91fa961fa292614a4581569b95ada2b3d30
root <admin@example.com>
2023-08-13 22:28:51 +0300

    added questions

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -5,16 +5,47 @@ class QuestionsScreen extends StatefulWidget {
 
   @override
   State<QuestionsScreen> createState() {
-   
-   return _QuestionsScreenState();
-   
+    return _QuestionsScreenState();
   }
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-
   @override
-  Widget build (context) {
-    return const Text('data');
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              'answer 1',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              'answer 2',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              'answer 3',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
