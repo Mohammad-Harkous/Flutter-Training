31619cdd94639d3e6dff9dfea99118d2d330912d
root <admin@example.com>
2023-08-25 19:07:11 +0300

    added color scheme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var KColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          useMaterial3: true,
-          scaffoldBackgroundColor: const Color.fromARGB(163, 255, 193, 7)),
+        useMaterial3: true,
+        colorScheme: KColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: KColorScheme.onPrimaryContainer,
+          foregroundColor: KColorScheme.primaryContainer,
+        )
+      ),
       home: const Expenses(),
     ),
   );
