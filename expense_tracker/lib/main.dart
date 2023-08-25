d513cffb85fb9d17f2406918ddf46877a7fea507
root <admin@example.com>
2023-08-25 19:25:07 +0300

    added style

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -15,7 +15,25 @@ void main() {
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: KColorScheme.onPrimaryContainer,
           foregroundColor: KColorScheme.primaryContainer,
-        )
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: KColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: KColorScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: KColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
