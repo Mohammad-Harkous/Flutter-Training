3adeaf5ad18cd933f91575e5c47099993b20a3b9
root <admin@example.com>
2023-09-03 23:12:57 +0300

    added category items

--- meals_app/lib/widgets/category_grid_item.dart
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -1,14 +1,34 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category,});
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
 
   final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          category.color.withOpacity(0.55),
+          category.color.withOpacity(0.9)
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      )),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+      ),
+    );
   }
 }
